70175929ab56a170ccceee71ac647370a97d39d3
tho thai <thotx1999@gmail.com>
2019-06-28 02:07:24 +0700

    dark era began

--- src/main/java/com/sol/dght003/api/CandidateAPI.java
+++ src/main/java/com/sol/dght003/api/CandidateAPI.java
@@ -5,13 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sol.dght003.dto.response.CandidateDto;
 import com.sol.dght003.service.CandidateService;
 
 @RestController
+@CrossOrigin("*")
+@RequestMapping("api")
 public class CandidateAPI {
 	
 	@Autowired
